e7128578a384c04727effcba318f08e6b7c28d6f
yczwky <vladimir@MacBook-Pro-Vladimir.local>
2024-11-13 18:05:39 +0300

    end2

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -8,27 +8,30 @@ def index(request):
     template = "blog/index.html"
     post_list = (
         Post.objects.filter(
-            is_published=True, category__is_published=True, pub_date__lte=timezone.now()
-        )
-        .order_by("-pub_date")[:5]
-        .select_related("category")
-    )
+            is_published=True,
+            category__is_published=True,
+            pub_date__lte=timezone.now()) .order_by("-pub_date")[
+            :5] .select_related("category"))
     context = {"post_list": post_list}
     return render(request, template, context)
 
 
 def post_detail(request, pk):
     template = "blog/detail.html"
-    post = get_object_or_404(Post, pk=pk, is_published=True, pub_date__lte=timezone.now(), category__is_published=True)
+    post = get_object_or_404(
+        Post,
+        pk=pk,
+        is_published=True,
+        pub_date__lte=timezone.now(),
+        category__is_published=True,
+    )
     return render(request, template, {"post": post})
 
 
 def category_posts(request, category_slug):
     template = "blog/category.html"
-    category = get_object_or_404(Category, slug=category_slug, is_published=True)
-    posts = Post.objects.filter(
-        category=category,
-        is_published=True,
-        category__is_published=True,
-    ).order_by("-pub_date")
+    category = get_object_or_404(
+        Category,
+        slug=category_slug,
+        is_published=True)
     return render(request, template, {"category": category})
