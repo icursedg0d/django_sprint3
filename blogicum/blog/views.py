7a47688fe0e17b8eba09e6b4c3eb17891bcd7fc8
yczwky <vladimir@MacBook-Pro-Vladimir.local>
2024-11-13 18:02:57 +0300

    end

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,26 +1,25 @@
 from django.utils import timezone
 
-from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Category
 
 
 def index(request):
     template = "blog/index.html"
-    # Фильтрация публикаций согласно условиям
-    context = (
+    post_list = (
         Post.objects.filter(
             is_published=True, category__is_published=True, pub_date__lte=timezone.now()
         )
         .order_by("-pub_date")[:5]
         .select_related("category")
     )
-    return render(request, template, {"posts": context})
+    context = {"post_list": post_list}
+    return render(request, template, context)
 
 
 def post_detail(request, pk):
     template = "blog/detail.html"
-    post = get_object_or_404(Post, pk=pk, is_published=True)
+    post = get_object_or_404(Post, pk=pk, is_published=True, pub_date__lte=timezone.now(), category__is_published=True)
     return render(request, template, {"post": post})
 
 
